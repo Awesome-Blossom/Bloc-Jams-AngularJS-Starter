b7e938836f194372fba88de2c60c8a8eb3a2989f
AB <acvarg@gmail.com>
2018-01-18 18:39:40 -0800

    Added Fixtures method fo collectionCtrl template

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,11 +1,11 @@
 (function (){
 
 	function AlbumCtrl(Fixtures){
-		this.albumData= angular.copy(albumPicasso)
+		this.albumData= Fixtures.getAlbum();
 	}
 
 	angular
 		.module('blocJams')
 		.controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
 
-})();
+})();	
